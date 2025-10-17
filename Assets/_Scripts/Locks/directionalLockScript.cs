2fd1c603749ee31035798fcc04189aef47813540
Daniel <87099848+unsophisticat@users.noreply.github.com>
2025-10-17 17:39:51 -0400

    Directional Lock Progress
    Implemented directional lock script logic.
    Implemented UI buttons and text displaying current input.
    Not tested, will do testing.

--- Assets/_Scripts/Locks/directionalLockScript.cs
+++ Assets/_Scripts/Locks/directionalLockScript.cs
@@ -1,19 +1,53 @@
 using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class directionalLockScript : MonoBehaviour
 {
-    public BoxCollider upTrigger;
-    public Boolean locked;
+    public Button upButton, downButton, leftButton,rightButton;
+    public bool locked;
+    public string targetSequence;
+    private int targetLength;
+
+    public Text seqText;
+    
+    public List<char> curSequence;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        curSequence = new List<char>();
+        targetLength = targetSequence.Length;
+        locked = true;
+
+        upButton.onClick.AddListener(() => AddSeqeunce('u'));
+        downButton.onClick.AddListener(() => AddSeqeunce('d'));
+        leftButton.onClick.AddListener(() => AddSeqeunce('l'));
+        rightButton.onClick.AddListener(() => AddSeqeunce('r'));
+    }
+
+    void AddSeqeunce(char dir)
+    {
+        curSequence.Add(dir);
+
+        seqText.text = curSequence.ToString();
+
+        if (curSequence.Count == targetLength)
+        {
+            if (curSequence.ToString() == targetSequence)
+            {
+                locked = false;
+            }
+            else 
+            {
+                curSequence.Clear();
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
