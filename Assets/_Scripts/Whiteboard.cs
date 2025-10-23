196a4c906326350787b5cc69733de4286a9a3f70
Fenway Powers <31604245+fenwaypowers@users.noreply.github.com>
2025-10-23 12:40:09 -0400

    Temporary fix for whiteboard color blending issue
    Changed the whiteboard and marker to chalkboard and chalk.
    
    Changed the chalkboard material to an unlit texture, which fixes the issue where the chalk color would blend with the chalkboard color.
    However, because it's unlit, this may look unnatural combined with the lighting in the final game, so we should continue to look for alternative solutions.
    
    added variable `fillColor` to `Whiteboard.cs`, which allows you to fill the chalkboard with a specified color rather than just a texture image.

--- Assets/_Scripts/Whiteboard.cs
+++ Assets/_Scripts/Whiteboard.cs
@@ -6,6 +6,8 @@ public class Whiteboard : MonoBehaviour
     [Tooltip("Texture applied to the whiteboard surface.")]
     public Texture2D texture;
 
+    public Color fillColor;
+    
     [Tooltip("Dimensions of the generated whiteboard texture.")]
     public Vector2 textureSize = new Vector2(2048, 2048);
 
@@ -25,6 +27,17 @@ public class Whiteboard : MonoBehaviour
         }
 
         texture = new Texture2D((int)textureSize.x, (int)textureSize.y);
+        
+        // Create an array with that color for every pixel
+        Color[] fillPixels = new Color[texture.width * texture.height];
+        
+        for (int i = 0; i < fillPixels.Length; i++)
+            fillPixels[i] = fillColor;
+        
+        // Set the pixels and apply the change
+        texture.SetPixels(fillPixels);
+        texture.Apply();
+
         _renderer.material.mainTexture = texture;
     }
 }
