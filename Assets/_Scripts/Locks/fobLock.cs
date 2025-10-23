7aa9fb8bc8616906c4740daafeb9ed42fcc542ca
Daniel <87099848+unsophisticat@users.noreply.github.com>
2025-10-23 06:32:51 -0400

    Fob Done
    Fob done, works.

--- Assets/_Scripts/Locks/fobLock.cs
+++ Assets/_Scripts/Locks/fobLock.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class fobLock : MonoBehaviour
 {
+    public GameObject fob;
+    public bool locked;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        locked = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == fob.name) {
+            locked = false;
+        }
     }
 }
