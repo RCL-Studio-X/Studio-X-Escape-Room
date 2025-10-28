0edc36575bf210465d3406d477592d46759bef34
Daniel <87099848+unsophisticat@users.noreply.github.com>
2025-10-28 13:44:05 -0400

    Stuff
    Things done on Monday, new lock, fixed audio error.

--- Assets/_Scripts/Locks/keyLock.cs
+++ Assets/_Scripts/Locks/keyLock.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 public class keyLock : MonoBehaviour
 {
     public bool locked = true;
-    private bool selected = false;
     public AudioSource audio;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -16,19 +15,9 @@ public class keyLock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {   
-        if (selected && gameObject.GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactors.XRSocketInteractor>().hasSelection) {
+        if (locked && gameObject.GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactors.XRSocketInteractor>().hasSelection) {
             locked = false;
             audio.Play();
         }
-    }
-
-    void onSelectEnter()
-    {
-        selected = true;
-    }
-
-    void onSelectExit()
-    {
-        selected = false;
     }
 }
