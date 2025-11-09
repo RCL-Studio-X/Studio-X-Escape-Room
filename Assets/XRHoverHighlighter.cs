20e120e0d1e9b3a425185a46dfbd110ce67a9d8b
sduprey18 <sherkeemduprey@gmail.com>
2025-11-09 13:33:43 -0500

    Hover functionality implemented - not tested
    The hover  logic and functionality is present, however the hands are stuck to the floor so it is unable to be tested, however the outline task might be done - sherkeem and jelena

---
+++ Assets/XRHoverHighlighter.cs
@@ -0,0 +1,26 @@
+
+using UnityEngine; 
+
+public class XRHoverHighlighter : MonoBehaviour
+{
+    [SerializeField] private string outlineLayerName = "Outline";
+
+    private int outlineLayer;
+    private int originalLayer;
+
+    void Awake()
+    {
+        outlineLayer = LayerMask.NameToLayer(outlineLayerName);
+        originalLayer = gameObject.layer;
+    }
+
+    public void OnHoverEnter(UnityEngine.XR.Interaction.Toolkit.HoverEnterEventArgs args)
+    {
+        gameObject.layer = outlineLayer;
+    }
+
+    public void OnHoverExit(UnityEngine.XR.Interaction.Toolkit.HoverExitEventArgs args)
+    {
+        gameObject.layer = originalLayer;
+    }
+}
