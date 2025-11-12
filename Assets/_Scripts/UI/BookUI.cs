1ccbd46c1ea8d54380f10e0073cd25726cb541ce
rayyyrayy <raymondaw02@gmail.com>
2025-11-12 17:34:15 -0500

    Next Button Functionality
    Added Book and UI the next button should switch the page text

---
+++ Assets/_Scripts/UI/BookUI.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BookUI : MonoBehaviour
+{
+    public List<TextMeshProUGUI> pageList;
+    public Button nextButton;
+    public Button prevButton;
+    public int currentPageIndex=0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+    
+    public void nextPage()
+    {
+        if (pageList[pageList.Count-1])
+        {
+            nextButton.interactable = false;
+        } else
+        {
+            pageList[currentPageIndex].enabled = false;
+            currentPageIndex += 1;
+            pageList[currentPageIndex].enabled = true;
+        }
+    }
+
+}
