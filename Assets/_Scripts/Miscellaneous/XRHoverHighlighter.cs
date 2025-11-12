3c30b95c51d87573d6fc935943b24b68a7839f7e
sduprey18 <sherkeemduprey@gmail.com>
2025-11-11 19:45:01 -0500

    Outline visible in game
    Outline works in game, but instantly goes back, creating somewhat of a blinking affect, i think the issue is with using layers but im not 100% sure. -sherkeem

--- Assets/_Scripts/Miscellaneous/XRHoverHighlighter.cs
+++ Assets/_Scripts/Miscellaneous/XRHoverHighlighter.cs
@@ -1,27 +1,23 @@
-
-using UnityEngine; 
-
-public class XRHoverHighlighter : MonoBehaviour
+using UnityEngine;
+public class XRHoverHighlighter : MonoBehaviour 
 {
     [SerializeField] private string outlineLayerName = "Outline";
-
-    private int outlineLayer;
+    private int outlineLayer; 
     private int originalLayer;
-
-    void Awake()
+    void Awake() 
     {
         outlineLayer = LayerMask.NameToLayer(outlineLayerName);
         originalLayer = gameObject.layer;
     }
-
-    public void OnHoverEnter(UnityEngine.XR.Interaction.Toolkit.HoverEnterEventArgs args)
+    public void OnHoverEnter(UnityEngine.XR.Interaction.Toolkit.HoverEnterEventArgs args) 
+    
     {
         gameObject.layer = outlineLayer;
         Debug.Log("Outline changed to: " + gameObject.layer + "!");
     }
-
     public void OnHoverExit(UnityEngine.XR.Interaction.Toolkit.HoverExitEventArgs args)
     {
         gameObject.layer = originalLayer;
     }
+
 }
