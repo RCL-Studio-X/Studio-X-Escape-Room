86c83054685c382eb0ceacbe0578a37f9f583233
rayyyrayy <raymondaw02@gmail.com>
2025-11-20 16:12:12 -0500

    changed script back

--- Assets/_Scripts/UI/PageUI.cs
+++ Assets/_Scripts/UI/PageUI.cs
@@ -5,24 +5,31 @@ using System;
 
 public class PageUI : MonoBehaviour
 {
+    //public Image backTab;
+    //public Image fowardTab;
     public Image leftTab;
     public Image rightTab;
-    private float leftPosX=-243;
+    private float leftPosX=-244;
+    private float leftPosY=235;
     private float rightPosX=246;
+    private float rightPosY=211;
+    private Vector3 leftTabPos;
+    private Vector3 rightTabPos;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {        
+    {
+        leftTabPos= new Vector3(leftPosX,leftPosY,0);
+        rightTabPos= new Vector3(rightPosX, rightPosY,0);
         
     }
+
     // Update is called once per frame
     void Update()
     {
-        // Only change X, keep current Y and Z
-        leftTab.transform.position = new Vector3(leftPosX, leftTab.transform.position.y, 0);
-        rightTab.transform.position = new Vector3(rightPosX, rightTab.transform.position.y, 0);
-
+        leftTab.transform.localPosition = leftTabPos;
+        rightTab.transform.localPosition = rightTabPos;
+        
     }
-
 
 }
