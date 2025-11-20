be9876b2ee20398e9e6004280cc3cbad4466dbc3
rayyyrayy <raymondaw02@gmail.com>
2025-11-20 16:16:09 -0500

    tried to do it to keep y pos

--- Assets/_Scripts/UI/PageUI.cs
+++ Assets/_Scripts/UI/PageUI.cs
@@ -1,35 +1,54 @@
 using UnityEngine;
-using UnityEngine.Rendering;
 using UnityEngine.UI;
-using System;
 
 public class PageUI : MonoBehaviour
 {
-    //public Image backTab;
-    //public Image fowardTab;
+    // Assign these in the Unity Inspector
     public Image leftTab;
     public Image rightTab;
-    private float leftPosX=-244;
-    private float leftPosY=235;
-    private float rightPosX=246;
-    private float rightPosY=211;
+
+    // Define the target X positions (Local Coordinates)
+    private float leftPosX = -244f;
+    private float rightPosX = 246f;
+    
+    // Variables to store the initial LOCAL Y positions
+    private float initialLeftLocalY;
+    private float initialRightLocalY;
+    
+    // Variables to hold the final target Vector3 positions
     private Vector3 leftTabPos;
     private Vector3 rightTabPos;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        leftTabPos= new Vector3(leftPosX,leftPosY,0);
-        rightTabPos= new Vector3(rightPosX, rightPosY,0);
-        
+        // 1. Store the CURRENT LOCAL Y positions of the tabs.
+        // This makes sure the tabs stay vertically aligned where you placed them.
+        if (leftTab != null)
+        {
+            initialLeftLocalY = leftTab.transform.localPosition.y;
+        }
+        if (rightTab != null)
+        {
+            initialRightLocalY = rightTab.transform.localPosition.y;
+        }
+
+        // 2. Pre-calculate the target Vector3 positions.
+        leftTabPos = new Vector3(leftPosX, initialLeftLocalY, 0f);
+        rightTabPos = new Vector3(rightPosX, initialRightLocalY, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        leftTab.transform.localPosition = leftTabPos;
-        rightTab.transform.localPosition = rightTabPos;
-        
+        // Set the local positions every frame using the pre-calculated vectors.
+        // This ensures they are constantly forced to the target (leftPosX, initialLocalY)
+        if (leftTab != null)
+        {
+            leftTab.transform.localPosition = leftTabPos;
+        }
+        if (rightTab != null)
+        {
+            rightTab.transform.localPosition = rightTabPos;
+        }
     }
-
 }
