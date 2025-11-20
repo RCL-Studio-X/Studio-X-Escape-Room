e128edb3b5f87db933aa279ece75060d4fcb6fd9
rayyyrayy <raymondaw02@gmail.com>
2025-11-20 15:27:56 -0500

    Update script to not need to update Y pos

--- Assets/_Scripts/UI/PageUI.cs
+++ Assets/_Scripts/UI/PageUI.cs
@@ -10,26 +10,23 @@ public class PageUI : MonoBehaviour
     public Image leftTab;
     public Image rightTab;
     private float leftPosX=-244;
-    private float leftPosY=235;
     private float rightPosX=246;
-    private float rightPosY=211;
     private Vector3 leftTabPos;
     private Vector3 rightTabPos;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        leftTabPos= new Vector3(leftPosX,leftPosY,0);
-        rightTabPos= new Vector3(rightPosX, rightPosY,0);
+        
         
     }
-
     // Update is called once per frame
     void Update()
     {
-        leftTab.transform.localPosition = leftTabPos;
-        rightTab.transform.localPosition = rightTabPos;
-        
+        // Only change X, keep current Y and Z
+        leftTab.transform.localPosition = new Vector3(leftPosX, leftTab.transform.localPosition.y, leftTab.transform.localPosition.z);
+        rightTab.transform.localPosition = new Vector3(rightPosX, rightTab.transform.localPosition.y, rightTab.transform.localPosition.z);
+
     }
 
 
