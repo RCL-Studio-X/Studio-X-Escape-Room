65a43276a461e319ec4c45db0a0a9f7ddc4457a8
Fenway Powers <fppowers@protonmail.com>
2026-01-20 16:12:30 -0500

    small edits

--- Assets/_Scripts/XR/XRControlsManager.cs
+++ Assets/_Scripts/XR/XRControlsManager.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
 using UnityEngine.XR.Interaction.Toolkit.Locomotion.Turning;
 using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
-using StudioX.EscapeRoom.UI;
+using StudioXRCL.EscapeRoom.UI;
 
 public class XRControlsManager : MonoBehaviour
 {
