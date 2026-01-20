6a232ffa4e65debe21a10e283d73d6474acf4767
Fenway Powers <fppowers@protonmail.com>
2026-01-20 16:09:43 -0500

    Update SelectBase.cs

--- Assets/_Scripts/Utilities/SelectBase.cs
+++ Assets/_Scripts/Utilities/SelectBase.cs
@@ -1,4 +1,15 @@
 using UnityEngine;
 
-[SelectionBase]
-public class SelectBase : MonoBehaviour {}
+namespace StudioXRCL.EscapeRoom.Utilities
+{
+    /// <summary>
+    /// Ensures that this GameObject is selected in the Unity Editor
+    /// when any of its child objects are clicked.
+    /// </summary>
+    [SelectionBase]
+    public class SelectBase : MonoBehaviour
+    {
+        // Intentionally empty.
+        // This component exists solely to control editor selection behavior.
+    }
+}
