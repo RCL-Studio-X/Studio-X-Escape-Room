c6515393bbe39d39c527bf97d923ffb0f3db4b0a
rayyyrayy <raymondaw02@gmail.com>
2026-03-25 17:16:04 -0400

    reverted back to raycast for a test

--- Assets/_Scripts/VFX/CreateTrail.cs
+++ Assets/_Scripts/VFX/CreateTrail.cs
@@ -6,72 +6,70 @@ namespace StudioXRCL.EscapeRoom.VFX
     {
         [Header("References")]
         public GameObject trailPrefab;
-        public Transform chalkTip;
 
         [Header("Board")]
         public LayerMask boardLayer;
+        public float maxDistance = 5f;
 
         [Header("Style")]
         public float width = 0.01f;
         public Color color = Color.black;
 
         private GameObject currentTrail;
-        private bool isDrawing = false; 
 
-        private Transform activeBoard; 
-
-        void OnTriggerEnter(Collider other)
+        void Update()
         {
-            if (((1 << other.gameObject.layer) & boardLayer) != 0)
+            if (!currentTrail)
+                return;
+
+            if (Physics.Raycast(transform.position,
+                                transform.forward,
+                                out RaycastHit hit,
+                                maxDistance,
+                                boardLayer))
             {
-                activeBoard = other.transform; 
-                StartTrail();
+                // Stick to board
+                currentTrail.transform.position = hit.point;
+
+                // Orient to board surface
+                currentTrail.transform.rotation =
+                    Quaternion.LookRotation(-hit.normal);
             }
         }
-
-        void OnTriggerExit(Collider other)
-        {
-            if (((1 << other.gameObject.layer) & boardLayer) != 0)
-            {
-                activeBoard = null; 
-                EndTrail();
-            }
-        }
-
-        void Update() 
-        {
-            if (isDrawing && currentTrail != null && activeBoard != null)
-            {
-                Vector3 localTipPos = activeBoard.InverseTransformPoint(chalkTip.position);
-                localTipPos.z = 0.001f; 
-                currentTrail.transform.position = activeBoard.TransformPoint(localTipPos);
-            }
-        }
-
 
         public void StartTrail()
         {
-            if (currentTrail) return;
-            
-            currentTrail = Instantiate(trailPrefab, chalkTip.position, chalkTip.rotation);
-            ApplySettings(currentTrail);
-            
-            isDrawing = true; 
+            if (currentTrail)
+                return;
+
+            if (Physics.Raycast(transform.position,
+                                transform.forward,
+                                out RaycastHit hit,
+                                maxDistance,
+                                boardLayer))
+            {
+                currentTrail = Instantiate(trailPrefab, hit.point, Quaternion.identity);
+                ApplySettings(currentTrail);
+            }
         }
 
         public void EndTrail()
         {
-            isDrawing = false; 
             currentTrail = null;
         }
+
         private void ApplySettings(GameObject trailObject)
         {
             var tr = trailObject.GetComponent<TrailRenderer>();
+
             tr.widthMultiplier = width;
             tr.startColor = color;
             tr.endColor = color;
+
+            // Important: keeps it flat
             tr.alignment = LineAlignment.TransformZ;
             tr.time = Mathf.Infinity;
         }
+
     }
 }
