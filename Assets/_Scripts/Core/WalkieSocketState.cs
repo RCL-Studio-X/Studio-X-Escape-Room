55281ae91beeca8a6368dd296c998041fb936b5b
Ouriya Boshi Levine <oboshile@u.rochester.edu>
2026-04-09 18:31:53 -0400

    Finished WalkieTalkie Socket and adhered to formatting conventions
    Added a hand-attached walkie socket using XR Interaction Toolkit.
    
    Socket is a child of Right Controller (WalkieHandSocket)
    Walkie uses XR Grab Interactable + custom WalkieSocketState script
    On socket:
    switches to Socketed interaction layer
    right hand cannot grab it
    left hand can still grab it
    On removal:
    switches back to WalkieTalkie
    Script
    Path: Assets/_Scripts/Core/WalkieSocketState.cs
    Namespace: StudioXRCL.EscapeRoom.Core
    Interaction Setup
    Left hand: Default + WalkieTalkie + Socketed
    Right hand: Default + WalkieTalkie
    Socket: WalkieTalkie

--- Assets/_Scripts/Core/WalkieSocketState.cs
+++ Assets/_Scripts/Core/WalkieSocketState.cs
@@ -3,28 +3,70 @@ using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
-public class WalkieSocketState : MonoBehaviour
+namespace StudioXRCL.EscapeRoom.Core
 {
-    public InteractionLayerMask freeLayer;
-    public InteractionLayerMask socketedLayer;
-
-    private XRGrabInteractable grab;
-
-    void Awake()
+    /// <summary>
+    /// Handles interaction layer switching for the walkie talkie when it is placed into or removed from a socket.
+    /// Ensures only the correct hand can interact with it while socketed.
+    /// </summary>
+    public class WalkieSocketState : MonoBehaviour
     {
-        grab = GetComponent<XRGrabInteractable>();
-    }
+        #region Public Variables
 
-    public void OnSelectEntered(SelectEnterEventArgs args)
-    {
-        if (args.interactorObject is XRSocketInteractor)
+        [Header("Interaction Layers")]
+
+        [Tooltip("Interaction layer used when the walkie is free and can be grabbed by both hands.")]
+        public InteractionLayerMask freeLayer;
+
+        [Tooltip("Interaction layer used when the walkie is in the socket and should only be interactable by the left hand.")]
+        public InteractionLayerMask socketedLayer;
+
+        #endregion
+
+        #region Private Variables
+
+        /// <summary> Reference to the XR Grab Interactable component. </summary>
+        private XRGrabInteractable _grabInteractable;
+
+        #endregion
+
+        #region Unity Methods
+
+        /// <summary>
+        /// Initializes component references.
+        /// </summary>
+        private void Awake()
         {
-            grab.interactionLayers = socketedLayer;
+            _grabInteractable = GetComponent<XRGrabInteractable>();
         }
-    }
 
-    public void OnSelectExited(SelectExitEventArgs args)
-    {
-        grab.interactionLayers = freeLayer;
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Called when the walkie is selected (grabbed or socketed).
+        /// Switches to socketed interaction layer if placed into a socket.
+        /// </summary>
+        /// <param name="args">Event data for the select enter event.</param>
+        public void OnSelectEntered(SelectEnterEventArgs args)
+        {
+            if (args.interactorObject is XRSocketInteractor)
+            {
+                _grabInteractable.interactionLayers = socketedLayer;
+            }
+        }
+
+        /// <summary>
+        /// Called when the walkie is deselected (released or removed from socket).
+        /// Restores the free interaction layer.
+        /// </summary>
+        /// <param name="args">Event data for the select exit event.</param>
+        public void OnSelectExited(SelectExitEventArgs args)
+        {
+            _grabInteractable.interactionLayers = freeLayer;
+        }
+
+        #endregion
     }
 }
